3ab56a599394ce05bf2fd254e99995b0fc23d044
Hani Ebrahimi <Hani@Hanis-MacBook-Air.local>
2017-03-11 10:01:22 -0500

    Initial commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,13 @@ class Product < ApplicationRecord
 	validates_numericality_of :price,
     greater_than: 49,
     message: "must be at least 50 cents"
+
+    validates_attachment_content_type :file, :content_type => [
+    "image/jpg",
+    "image/jpeg",
+    "image/png",
+    "image/gif",
+    "application/pdf",
+    "application/zip"
+]
 end
