e14477e41dfdfad8fb56ee2549acd51cf0bab5e6
Hani Ebrahimi <Hani@Hanis-MacBook-Air.local>
2017-03-12 23:28:01 -0400

    custom form and sideKiq

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
+ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6789' 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
