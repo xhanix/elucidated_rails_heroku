6bc2ece182cc126fc4a42cddee9a1e771e13fbe4
Hani Ebrahimi <Hani@Hanis-MacBook-Air.local>
2017-03-13 09:32:23 -0400

    added sidekiq worker

---
+++ app/workers/stripe_charger_worker.rb
@@ -0,0 +1,13 @@
+class StripeChargerWorker
+  include Sidekiq::Worker
+
+  def perform(guid)
+		ActiveRecord::Base.connection_pool.with_connection do
+			sale = Sale.find_by(guid: guid)
+			puts "***** worker process *****"
+			return unless sale
+			sale.process!
+
+		end 
+	end
+end
