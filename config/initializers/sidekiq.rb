9f7adf52475fe4655f05677a95adf78d543d5a11
Hani Ebrahimi <Hani@Hanis-MacBook-Air.local>
2017-03-13 19:47:23 -0400

    configuring sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,21 @@
 
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDISTOGO_URL'], size: 2 }
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV["REDISTOGO_URL"], namespace: :resque }
+  config.reliable_fetch!
+
+  database_url = ENV['DATABASE_URL']
+  if database_url
+    ENV['DATABASE_URL'] = "#{database_url}?pool=250"
+    ActiveRecord::Base.establish_connection
   end
 
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDISTOGO_URL'], size: 44 }
+  $elastic = Elasticsearch::Client.new
+  Stretchy.client = $elastic
 end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV["REDISTOGO_URL"], namespace: :resque }
+end
+
+Sidekiq::Client.reliable_push! unless Rails.env.test?
