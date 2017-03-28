3cd991ff7ffea9eedce20da27e53e66b79fc7492
Hani Ebrahimi <Hani@Hanis-MacBook-Air.local>
2017-03-28 10:11:35 -0400

    fixes parse messenger

--- app/workers/message_parse_worker.rb
+++ app/workers/message_parse_worker.rb
@@ -8,7 +8,7 @@ class MessageParseWorker
 			user = User.find_by(email: email)
 			#subscription = user.subscriptions.find_by(guid: guid)
 			sub_status = 'Active' # use instead of subscription.status to leave Parse account active for now
-			return unless subscription
+			#return unless subscription
 			Publisher.publish({username: user.fullname, email: user.email, description: user.description, licenseState: sub_status})
 		end 
 	end
